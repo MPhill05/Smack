c8a834c2739d8899fd3cf2f2413007a7cc30608d
Mark Phillips <mphill05@gmail.com>
2017-11-09 00:42:36 -0800

    login setup

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mark Phillips on 11/9/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Mark Phillips on 11/9/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"

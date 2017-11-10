b5bf333d3441db297f633f27835e63e1e14017dd
Mark Phillips <mphill05@gmail.com>
2017-11-10 01:56:20 -0800

    create user with image, update color and circle image updating

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Mark Phillips on 11/10/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+}

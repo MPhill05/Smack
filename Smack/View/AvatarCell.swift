f73f1e12312dd6249416c8bb9c77e37aa51998e0
Mark Phillips <mphill05@gmail.com>
2017-11-10 00:09:39 -0800

    setup page for avatar selector and added the dark squares for it

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Mark Phillips on 11/10/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
